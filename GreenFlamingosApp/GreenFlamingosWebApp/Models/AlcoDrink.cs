b5d9d1af159c34be8526b1173eab14a24375b53f
Jakub Gruszczyk <j.gruszczyk96@gmail.com>
2022-12-01 10:15:20 +0100

    GreenFlamingos Porject

---
+++ GreenFlamingosApp/GreenFlamingosWebApp/Models/AlcoDrink.cs
@@ -0,0 +1,34 @@
+﻿using GreenFlamingos.Model.Drinks;
+using GreenFlamingos.Model;
+
+namespace GreenFlamingosWebApp.Models
+{
+    public class AlcoDrink : Drink
+    {
+        public AlcoDrink()
+        {
+            DrinkType = "Drink";
+        }
+        //public AlcoDrink(string name, User owner, string mainIgredient, int capacity, double alcoholContent, int calories, List<string> ingriedients, string description, List<string> preparation)
+        //{
+        //    Random rnd = new Random();
+        //    Id = rnd.Next(100000, 299999);
+        //    //Here is needed a condition to check if created number is already used by other AlcoDrink
+
+        //    DrinkType = "Drink";
+        //    Name = name;
+        //    Owner = owner;
+        //    MainIngredient = mainIgredient;
+        //    if (capacity < 100 || capacity > 500)
+        //        throw new ArgumentOutOfRangeException();
+        //    else
+        //        Capacity = capacity;
+
+        //    AlcoholContent = alcoholContent;
+        //    Calories = calories;
+        //    Ingredients = ingriedients;
+        //    Description = description;
+        //    Preparation = preparation;
+        //}
+    }
+}
